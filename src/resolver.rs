c42337f9a66408f99b45c4ce82c334817b9fc219
aziz.karabashov <aziz.karabashov@dsr-corporation.com>
2025-03-06 14:49:20 +0500

    ASDK-288: Use WasmNotSend and WasmNotSync instead of Send and Sync
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

--- src/resolver.rs
+++ src/resolver.rs
@@ -2,9 +2,10 @@ use async_trait::async_trait;
 use jsonwebtoken::{DecodingKey, Header};
 
 use crate::error::Result;
+use crate::utils::{WasmNotSend, WasmNotSync};
 
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-pub trait KeyResolver: Sync + Send {
+pub trait KeyResolver: WasmNotSync + WasmNotSend {
     async fn resolve(&self, input: &str, header: &Header) -> Result<DecodingKey>;
 }
--- src/signer.rs
+++ src/signer.rs
@@ -1,9 +1,10 @@
 use async_trait::async_trait;
 use crate::error::Result;
+use crate::utils::{WasmNotSend, WasmNotSync};
 
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-pub trait SDJWTSigner: Sync + Send {
+pub trait SDJWTSigner: WasmNotSync + WasmNotSend {
     /// Return the source algorithm.
     fn algorithm(&self) -> &str;
 
